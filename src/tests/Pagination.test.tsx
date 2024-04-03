9397f330932cae3aec8fe887f639f486309883d6
Arek Daszkowski <daszkowskiarek145@gmail.com>
2024-04-03 23:12:35 +0200

    improving tests

--- src/tests/Pagination.test.tsx
+++ src/tests/Pagination.test.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import * as reduxHooks from '../store/store';
 import { PaginationComponent } from '../components/Pagination/Pagination';
-import { setCurrentPage } from '../store/slices/productsSlice';
+import * as reduxHooks from '../store/store';
+import { BrowserRouter } from 'react-router-dom';
 
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn()
+}));
 jest.mock('../store/store', () => ({
-  useAppDispatch: jest.fn(),
   useAppSelector: jest.fn()
 }));
 
@@ -14,12 +17,23 @@ describe('PaginationComponent Tests', () => {
   test('renders correctly and can change pages', async () => {
     const mockUseAppSelector = reduxHooks.useAppSelector as jest.Mock;
     mockUseAppSelector.mockReturnValue({ totalPages: 5, currentPage: 1 });
-    const mockDispatch = jest.fn();
-    const mockUseAppDispatch = reduxHooks.useAppDispatch as jest.Mock;
-    mockUseAppDispatch.mockReturnValue(mockDispatch);
-    render(<PaginationComponent />);
+
+    const mockNavigate = jest.fn();
+    (
+      jest.requireMock('react-router-dom').useNavigate as jest.Mock
+    ).mockReturnValue(mockNavigate);
+
+    render(
+      <BrowserRouter>
+        <PaginationComponent />
+      </BrowserRouter>
+    );
+
     const nextPageButton = screen.getByRole('button', { name: 'Go to page 2' });
-    await waitFor(() => userEvent.click(nextPageButton));
-    expect(mockDispatch).toHaveBeenCalledWith(setCurrentPage(2));
+    await userEvent.click(nextPageButton);
+
+    expect(mockNavigate).toHaveBeenCalledWith('/products?page=2', {
+      replace: true
+    });
   });
 });
